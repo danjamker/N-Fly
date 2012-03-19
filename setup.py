af45483b0b6158a8c60e602eca82c1b140b4e8ff
Daniel J. Kershaw <danjamker@gmail.com>
2012-03-19 23:06:12 +0000

    Chnging the setup fil.

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ Created on 12 Mar 2012
 @author: Daniel Kershaw
 '''
 from distutils.core import setup
-import py2exe
+#import py2exe
 
 files = ["main/*"]
 
 setup(name = "Project N-Fly",
     version = "0.1",
-    description = "NLP Keyword Extraction on Online Content for Advertising Systems",
+    description = "NLP Keyword Extraction of Online Content for an Online Advertising Systems",
     author = "Daniel Kershaw",
     author_email = "d.kershaw1@lancaster.ac.uk",
     url = "http://github.com/danjamker/N-Fly",
     long_description = """Really long text here.""",
-    console = ["main/main.py"]
+    py_modules = ['main\main.py']
 ) 
